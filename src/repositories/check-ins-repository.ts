42821e2c5b9f514b358f4f59186a19a82a479fa8
yuribodo <yemotalara@gmail.com>
2025-02-18 23:55:53 -0400

    feat: create a repository for checkins

---
+++ src/repositories/check-ins-repository.ts
@@ -0,0 +1,7 @@
+import { CheckIn, Prisma } from '@prisma/client'
+
+export interface CheckInsRepository {
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+}
