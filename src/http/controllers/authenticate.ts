c23f0fa342d9c9972b558f0c606a49902bfa8901
yuribodo <yemotalara@gmail.com>
2025-02-18 23:54:00 -0400

    feat: create a controller to use authenticate

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,33 @@
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticatUseCase } from '@/use-cases/factories/make-authenticate-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const authenticaterUseCase = makeAuthenticatUseCase()
+
+    await authenticaterUseCase.execute({
+      email,
+      password,
+    })
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: error.message })
+    }
+
+    throw error
+  }
+
+  return reply.status(200).send()
+}
