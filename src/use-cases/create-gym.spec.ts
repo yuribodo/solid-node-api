e3e3e385ecb86e7b242978e5eee1ead48e1872b3
yuribodo <yemotalara@gmail.com>
2025-02-19 23:32:34 -0400

    feat: create test for gym use case

---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { CreateGymUseCase } from './create-gym'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Gym Use case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: -27.2092052,
+      longitude: -49.6401091,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
